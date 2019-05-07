8f9da1260ba27cb33577a891501d6bdf3b582ec2
Misha <gobanovwork@gmail.com>
2019-05-07 17:11:37 +0200

    new plugin

--- plugins/redmine_meetings-master/db/migrate/003_meetings_notification.rb
+++
@@ -1,9 +0,0 @@
-class MeetingsNotification < ActiveRecord::Migration
-  def up
-    add_column :meetings, :notify_participants, :boolean, :default => false, :null => false
-  end
-
-  def down
-    remove_column :meetings, :notify_participants
-  end
-end
