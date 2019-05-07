f7a3d5bfa48edeb623fb17010eab45627bd75186
Misha <gobanovwork@gmail.com>
2019-05-07 17:10:21 +0200

    new plugin

---
+++ plugins/redmine_meetings-master/app/models/meeting.rb
@@ -0,0 +1,128 @@
+class Meeting < ActiveRecord::Base
+
+  belongs_to :project
+  belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
+
+  validates_presence_of :start_date, :end_date, :subject
+  validates_length_of :subject, :maximum => 255
+  validates_length_of :location, :maximum => 255
+
+  acts_as_attachable :delete_permission => :manage_meeting
+
+  acts_as_watchable
+
+  acts_as_searchable :columns => ["#{table_name}.subject", "#{table_name}.description"],
+  :include => [:project]
+
+  acts_as_event :title => Proc.new {|o| "#{l(:label_title_meeting)} ##{o.id}: #{format_time(o.start_date)} - #{o.subject}" },
+  :description => Proc.new {|o| "#{o.description}"},
+  :datetime => :updated_on,
+  :type => 'meetings',
+  :url => Proc.new {|o| {:controller => 'meetings', :action => 'show_meeting', :id => o.id} }
+
+  acts_as_activity_provider :type => 'meetings',
+  :timestamp => "#{table_name}.updated_on",
+  :author_key => "#{table_name}.author_id",
+  :permission => :view_meetings,
+  :find_options => {:joins => "LEFT JOIN #{Project.table_name} ON #{Project.table_name}.id = #{table_name}.project_id"}
+
+  scope :visible, lambda {|*args| { :include => :project,
+                                          :conditions => Project.allowed_to_condition(args.shift || User.current, :view_meetings, *args) } }
+
+  def visible?(user=User.current)
+    !user.nil? && user.allowed_to?(:view_meetings, project)
+  end
+
+  def author
+    author_id ? User.find(:first, :conditions => "users.id = #{author_id}") : nil
+  end
+
+  def project
+    Project.find(:first, :conditions => "projects.id = #{project_id}")
+  end
+
+  def start_date_date
+    start_date.to_date()
+  end
+
+  def start_date_time
+    start_date.to_time()
+  end
+
+  def end_date_date
+    end_date.to_date()
+  end
+
+  def end_date_time
+    end_date.to_time()
+  end
+
+  def <=>(meeting)
+    start_date <=> meeting.start_date
+  end
+
+  def to_s
+    "##{id}: #{start_date} - #{subject}"
+  end
+
+  def css_classe
+    if web
+      "webmeeting"
+    else
+      "meeting"
+    end
+  end
+
+  def deliver()
+    recipients = { author.language => [ author.mail ] }
+    if notify_participants
+      watcher_users.each do |w|
+        recipients[w.language] = update_recipients(recipients, w.language, w.mail)
+      end
+    end
+    recipients.each do |language,rec|
+      MeetingMailer.send_meeting(self, rec, language).deliver
+    end
+    return true
+  end
+  
+  def deliver_cancel()
+    recipients = { author.language => [ author.mail ] }
+    if notify_participants
+      watcher_users.each do |w|
+        recipients[w.language] = update_recipients(recipients, w.language, w.mail)
+      end
+    end
+    recipients.each do |language,rec|
+      MeetingMailer.cancel_meeting(self, rec, language).deliver
+    end
+    return true
+  end
+
+  def validate
+    valide = true
+    if !start_date.is_a?(DateTime)
+      valide = false
+    end
+    if !end_date.is_a?(DateTime)
+      valide = false
+    end
+    if start_date > end_date
+      valide = false
+    end
+
+    if !valide
+    errors.add(:end_date_date, :greater_than_start_date)
+    end
+    
+  end
+
+  private
+
+  def update_recipients (list, lang, mail)
+    mails = list[lang]
+    mails ||= []
+    mails << mail
+  end
+
+end
---
+++ plugins/redmine_meetings-master/app/models/meeting_mailer.rb
@@ -0,0 +1,167 @@
+require "tzinfo"
+require 'ri_cal'
+
+class MeetingMailer < Mailer
+
+  def send_doodle(doodle, rec, language)
+    set_language_if_valid language
+    message_id doodle
+    references doodle
+    redmine_headers 'Meeting-Doodle-ID' => doodle.id
+    sub = "[#{doodle.project.name} - #{t :label_title_doodle} #{doodle.id}]#{doodle.title}"
+    @doodle = doodle
+    @doodle_url = url_for(:controller => 'meetings', :action => 'show_doodle', :id => doodle)
+    mail :to => rec,
+      :subject => sub
+  end
+
+  def send_invalid_answer(doodle, rec, language)
+    set_language_if_valid language
+    references doodle
+    redmine_headers 'Meeting-Doodle-ID' => doodle.id
+    sub = "FAILED:[#{doodle.project.name} - #{t :label_title_doodle} #{doodle.id}]#{doodle.title}"
+    @doodle = doodle
+    mail :to => rec,
+      :subject => sub
+  end
+
+  def send_ak_answer(response, rec, language)
+    set_language_if_valid language
+    doodle = response.meeting_doodle
+    references doodle
+    redmine_headers 'Meeting-Doodle-ID' => doodle.id
+    accepted = []
+    doodle.tab_options.zip(response.answers).each do |choice, selected|
+      accepted << "[#{choice.strip}]" if selected
+    end
+    sub = "SUCCESS:[#{doodle.project.name} - #{t :label_title_doodle} #{doodle.id}]#{doodle.title}"
+
+    @doodle = doodle
+    @response = response
+    @accepted = accepted.join(', ')
+    @doodle_url = url_for(:controller => 'meetings', :action => 'show_doodle', :id => doodle)
+
+    mail :to => rec,
+      :subject => sub
+  end
+
+  def receive_answer(answer)
+    doodle = answer.meeting_doodle
+    name = answer.author.mail ? answer.author.name : answer.name
+    set_language_if_valid doodle.author.language
+    sub = "ANSWER:[#{doodle.project.name} - #{t :label_title_doodle} #{doodle.id}]#{doodle.title}"
+    @doodle = doodle
+    @name = name
+    @doodle_url = url_for(:controller => 'meetings', :action => 'show_doodle', :id => doodle)
+
+    mail :to => doodle.author.mail,
+      :subject => sub
+  end
+
+  def send_meeting(meeting, rec, language)
+    set_language_if_valid language
+
+    tzid = User.current.time_zone ? User.current.time_zone : ActiveSupport::TimeZone[Setting.plugin_redmine_meetings['meeting_timezone']]
+    desc = ''
+    desc << meeting.description
+    if meeting.web
+      desc << "\n"
+      desc << l(:content_meeting_email3, url_for(:controller => 'meetings', :action => 'join_conference', :project_id => meeting.project))
+    end
+    cal = RiCal.Calendar do |cal|
+      cal.prodid           = "BIOPROJ"
+      cal.method_property  = ":REQUEST"
+      cal.event do |event|
+        event.dtstamp     = DateTime.now.utc
+        event.summary     = meeting.subject
+        event.description = desc
+        event.dtstart     = meeting.start_date.utc
+        event.dtend       = meeting.end_date.utc
+        event.location    = meeting.web ? l(:field_meeting_web) : meeting.location
+        meeting.watcher_users.collect.sort.each do |user|
+          event.add_attendee  user.mail
+        end
+        event.organizer   = meeting.author.mail
+        event.uid         = "B10AA0B0-0000-0000-#{"%012d" % meeting.id}"
+        event.status      = "CONFIRMED"
+        event.class_property = ":PUBLIC"
+        event.priority    = 5
+        event.transp      = "OPAQUE"
+        event.alarm do
+          description "REMINDER"
+          action "DISPLAY"
+          trigger_property ";RELATED=START:-PT5M"
+        end
+      end
+    end
+
+
+    @author = User.anonymous
+
+    sub = "[#{meeting.project.name} - #{t :label_title_meeting} #{meeting.start_date.strftime('%F')}] #{meeting.subject}"
+    @meeting = meeting
+    @conf_url = url_for(:controller => 'meetings', :action => 'join_conference', :project_id => meeting.project)
+    @meeting_url = url_for(:controller => 'meetings', :action => 'show_meeting', :id => meeting)
+    @meeting_tz = User.current.time_zone ? User.current.time_zone : ActiveSupport::TimeZone[Setting.plugin_redmine_meetings['meeting_timezone']]
+    #content_type "multipart/alternative"
+
+    attachments['meeting.ics'] = {:mime_type => "text/calendar", :content => cal.to_s}
+    mail :to => rec,
+      :subject => sub,
+      :reply_to => meeting.author.mail
+  end
+
+def cancel_meeting(meeting, rec, language)
+  set_language_if_valid language
+
+  tzid = User.current.time_zone ? User.current.time_zone : ActiveSupport::TimeZone[Setting.plugin_redmine_meetings['meeting_timezone']]
+  desc = ''
+  desc << meeting.description
+  if meeting.web
+    desc << "\n"
+    desc << l(:content_meeting_email3, url_for(:controller => 'meetings', :action => 'join_conference', :project_id => meeting.project))
+  end
+  cal = RiCal.Calendar do |cal|
+    cal.prodid           = "BIOPROJ"
+    cal.method_property  = ":CANCEL"
+    cal.event do |event|
+      event.dtstamp     = DateTime.now.utc
+      event.summary     = meeting.subject
+      event.description = desc
+      event.dtstart     = meeting.start_date.utc
+      event.dtend       = meeting.end_date.utc
+      event.location    = meeting.web ? l(:field_meeting_web) : meeting.location
+      meeting.watcher_users.collect.sort.each do |user|
+        event.add_attendee  user.mail
+      end
+      event.organizer   = meeting.author.mail
+      event.uid         = "B10AA0B0-0000-0000-#{"%012d" % meeting.id}"
+      event.status      = "CANCELLED"
+      event.class_property = ":PUBLIC"
+      event.priority    = 5
+      event.transp      = "OPAQUE"
+      event.alarm do
+        description "REMINDER"
+        action "DISPLAY"
+        trigger_property ";RELATED=START:-PT5M"
+      end
+    end
+  end
+
+
+  @author = User.anonymous
+
+  sub = "[#{meeting.project.name} - #{t :label_title_meeting} #{meeting.start_date.strftime('%F')}] #{meeting.subject}"
+  @meeting = meeting
+  @conf_url = url_for(:controller => 'meetings', :action => 'join_conference', :project_id => meeting.project)
+  @meeting_url = url_for(:controller => 'meetings', :action => 'show_meeting', :id => meeting)
+  @meeting_tz = User.current.time_zone ? User.current.time_zone : ActiveSupport::TimeZone[Setting.plugin_redmine_meetings['meeting_timezone']]
+  #content_type "multipart/alternative"
+
+  attachments['meeting.ics'] = {:mime_type => "text/calendar", :content => cal.to_s}
+  mail :to => rec,
+    :subject => sub,
+    :reply_to => meeting.author.mail
+end
+
+end
