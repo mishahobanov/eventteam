8e3980e848ecc750966ae547016163eef9fa02e8
Misha <gobanovwork@gmail.com>
2019-05-07 21:30:51 +0200

    new plugin

--- plugins/redmine_meeting/test/functional/meetings_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../test_helper', __FILE__)
-
-class MeetingsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
