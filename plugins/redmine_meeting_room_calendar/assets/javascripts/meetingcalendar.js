2ac7982f84b40ecfe2c0b1a65d8384a49f6b6e33
Misha <gobanovwork@gmail.com>
2019-05-09 14:06:12 +0200

    plugin update

--- plugins/redmine_meeting_room_calendar/assets/javascripts/meetingcalendar.js
+++ plugins/redmine_meeting_room_calendar/assets/javascripts/meetingcalendar.js
@@ -263,15 +263,6 @@
       Author: shiju@qburst.com
       Description: checking whether the current user is same as the author of event
       */
-      var isCurrentUserid =function(req) {
-          var current_user_id = req.session.user_id;
-        if (current_user_id == 1){
-          alert("asdasd");
-        }
-        else {
-              alert("2222");
-        }
-      };
 
       var isCurrentUser = function(event_author_id, event_assigned_to_id) {
           var current_user_id = $('#author_id').val();
