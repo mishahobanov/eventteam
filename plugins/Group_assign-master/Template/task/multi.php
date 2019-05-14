c91c0de94ffe45840d41c3d578e90409839ce41e
Misha <gobanovwork@gmail.com>
2019-05-14 17:05:14 +0200

    new plugin

--- plugins/Group_assign-master/Template/task/multi.php
+++
@@ -1,6 +0,0 @@
-                    <?php if ($task['owner_ms'] > 0 && count($this->task->multiselectMemberModel->getMembers($task['owner_ms'])) > 0) : ?>
-                    <li>
-                        <strong><?= t('Other Assignees:') ?></strong>
-                    </li>
-                    <?= $this->helper->smallAvatarHelperExtend->smallMultiple($task['owner_ms'], 'avatar-inline') ?>
-                    <?php endif ?>
