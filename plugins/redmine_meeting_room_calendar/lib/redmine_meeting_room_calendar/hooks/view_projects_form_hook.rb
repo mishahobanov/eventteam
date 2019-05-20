db9752b980f0aec116bbcd61cdc9078cb7876a9e
Misha <gobanovwork@gmail.com>
2019-05-20 16:41:44 +0200

    delete

--- plugins/redmine_meeting_room_calendar/lib/redmine_meeting_room_calendar/hooks/view_projects_form_hook.rb
+++
@@ -1,7 +0,0 @@
-module RedmineMeetingRoomCalendar
-  module Hooks
-    class ViewProjectsFormHook < Redmine::Hook::ViewListener
-      render_on(:view_projects_form, :partial => 'projects/project_meeting_rooms', :layout => false)
-    end
-  end
-end
