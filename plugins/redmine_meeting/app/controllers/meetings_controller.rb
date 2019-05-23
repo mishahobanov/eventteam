b94be5a18e2ad7f7d3b76555173e31a6fd84f254
Misha <gobanovwork@gmail.com>
2019-05-23 06:30:03 +0200

    upd

--- plugins/redmine_meeting/app/controllers/meetings_controller.rb
+++ plugins/redmine_meeting/app/controllers/meetings_controller.rb
@@ -189,23 +189,7 @@ class MeetingsController < ApplicationController
         end
     end
     def meeting_perms
-        params.require(:meeting).permit(:subject,
-                                        :location,
-                                        :location_online,
-                                        :project_id,
-                                        :user_id,
-                                        :recurring_type,
-                                        :days_recurring,
-                                        :weekly_recurring,
-                                        :monthly_recurring,
-                                        :end_time,
-                                        :start_time,
-                                        :status,
-                                        :date,
-                                        :end_date,
-                                        :agenda,
-                                        :custom_field_values,
-                                        :meeting_minutes)
+        params.require(:meeting).permit(:subject,:location,:location_online,:project_id,:user_id,:recurring_type,:days_recurring,:weekly_recurring,:monthly_recurring,:end_time,:start_time,:status,:date,:end_date,:agenda,:custom_field_values,:meeting_minutes)
     end
     def get_meeting
         @meeting = Meeting.find(params[:id])
